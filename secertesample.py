98fb89b590b79d5c3611dab9ccdd48830808d571
rahulsoniceng <rahulsoniceng@gmail.com>
2026-04-06 15:54:03 +0530

    Create secertesample.py

---
+++ secertesample.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Loads variables from .env file
+
+api_key = os.getenv("API_TOKEN")
+print(f"Using API key: {ZaCELgL.0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx}")
