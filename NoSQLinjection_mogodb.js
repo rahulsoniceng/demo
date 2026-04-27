e4ca2ca23eafc4800cc0604e1088dd5adba7f8cd
HCLTech <rahulsoniceng@gmail.com>
2026-04-27 11:50:09 +0530

    Update NoSQLinjection_mogodb.js

--- NoSQLinjection_mogodb.js
+++ NoSQLinjection_mogodb.js
@@ -1,5 +1,4 @@
-
-const express = requirel ('express');
+const express = require('express');
 const config = require('../config')
 const router = express.Router()
 
@@ -8,16 +7,16 @@ const url = config.MONGODB_URI;
 
 router.post('/customers/register', async (req, res) => {
 
-  const client = await MongoClient.connect(url, { useNewUrlParser: true })
-.catch(err => { console.log(err); });
+  const client = await MongoClient.connect(url, {useNewUrlParser: true})
+.catch(err => {console.log(err);});
 if (!client) {
-return res.json({ status: "Error" });
+return res.json({status: "Error"});
 }
 const db = client.db(config.MONGODB_DB_NAME);
 const customers = db.collection("customers")
 
-let myob] = {name: req.body.name, address: req.body.address};
-customers.insertOne(myobj. function (err) {
+let myob] = {name:req.body.name, address:req.body.address};
+customers.insertOne(myobj,function (err) {
 if (err) throw err;
 console log("user registered");
 res.json({ status: "success", "message": "user inserted"})
