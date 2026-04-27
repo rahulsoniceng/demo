e5fe18a833710c61097d84106d8cc91874646d50
HCLTech <rahulsoniceng@gmail.com>
2026-04-27 10:41:44 +0530

    Update mysqlexample.js
    comments

--- mysqlexample.js
+++ mysqlexample.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-
+//
 const config = require('../../config')
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
