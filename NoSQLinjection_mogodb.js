f5eed0e04b84edfac729b627e9caae47922a3007
HCLTech <rahulsoniceng@gmail.com>
2026-04-27 11:42:34 +0530

    Update NoSQLinjection_mogodb.js

--- NoSQLinjection_mogodb.js
+++ NoSQLinjection_mogodb.js
@@ -1,21 +1,26 @@
 
-const express = requirel express);
-const config = require(./config)
-const router = express.RouterQ
-const MongoClient = requirel'mongodb').MongoClient;
+const express = requirel ('express');
+const config = require('../config')
+const router = express.Router()
+
+const MongoClient = require('mongodb').MongoClient;
 const url = config.MONGODB_URI;
-router.post(/customers/register, async (req, res) => (
-const client = await MongoClient.connect(url, ( useNewUrlParser: true I)
-catcherr => ( console.log(err): ):
-if (!client) [
-return res-json(i status: "Error" I):
+
+router.post('/customers/register', async (req, res) => {
+
+  const client = await MongoClient.connect(url, { useNewUrlParser: true })
+.catch(err => { console.log(err); });
+if (!client) {
+return res.json({ status: "Error" });
 }
-const db = client.db(config.MONGODB_DB_NAME):
+const db = client.db(config.MONGODB_DB_NAME);
 const customers = db.collection("customers")
-let myob] = (name: reg.body.name, address: reg.body.address:
-customers.insertOne(myobj. function (err) i
+
+let myob] = {name: req.body.name, address: req.body.address};
+customers.insertOne(myobj. function (err) {
 if (err) throw err;
-console log("user registered"):
-res.json(i status: "success", "message": "user inserted" 1) db.close();
+console log("user registered");
+res.json({ status: "success", "message": "user inserted"})
+db.close();
 });
 })
